2dc6342e530aa504687326d464c0157e82f3f2b7
Rajeev Kilaru <rkilaru@patriots.me>
2021-06-28 19:48:12 -0700

    Modifications allows a non-zero file as a file lock for the process

--- check_file_lock.sh
+++ check_file_lock.sh
@@ -1,33 +1,44 @@
+########################################
+# File: check_file_lock.sh             #
+# Author:  Rajeev Kilaru               #
+# Date:  06/28/2021                    #
+# Purpose: Simple non-zero bytes size  #
+#      file as lock and with a time out#
+########################################
+
 #! /usr/bin/bash 
 
 function status_check {
 
-ITERATIONS=15   # change this one to suite your requirement
-SLEEP=2          # time interval between each iterations in seconds
-STATUS_FILE=status.txt # if you are waiting for a for a job complettion, with file as lock.
+  ITERATIONS=15   # change this one to suite your requirement
+  SLEEP=5          # time interval between each iterations in seconds
+  STATUS_FILE=/tmp/status.txt # if you are waiting for a for a job complettion, with file as lock.
 
-FLAG=false
-COUNT=0
-while [ ${COUNT} -lt ${ITERATIONS} ]
-do
-  STATUS=`cat ${STATUS_FILE} 2>/dev/null`
-  if [ "${STATUS}" = "SUCCESS" ] 
-    then
+  FLAG=false
+  COUNT=0
+  while [ ${COUNT} -lt ${ITERATIONS} ]
+  do
+    # Break the loop if the file is none zero size
+    if test -s ${STATUS_FILE} ; then
       FLAG=true
       break
-  fi
-  sleep ${SLEEP}
-  echo "End of iteration: $COUNT"
-  ((COUNT=COUNT+1))
-done
+    fi
+
+    sleep ${SLEEP}
+    echo "End of iteration: $COUNT"
+    ((COUNT=COUNT+1))
+
+  done
 
   ${FLAG} && return 0 || return 1
 
 }
 
+STATUS_FILE=/tmp/status.txt # if you are waiting for a for a job complettion, with file as lock.
+rm ${STATUS_FILE}
+
 # call funciton 
 status_check
-
 # get return code
 rc=$?
 
